f272afd6e8d37af916f6963acc9a9109d1c911fd
PriyanshuSharma0326 <ps940016@gmail.com>
2024-02-29 19:54:24 +0530

    catching all segments in routing

---
+++ src/app/docs/[[...slug]]/page.js
@@ -0,0 +1,13 @@
+function Docs({ params }) {
+    if(params.slug?.length === 2) {
+        return <h1>View Docs for Feature {params.slug[0]} and concept {params.slug[1]}</h1>;
+    }
+    else if (params.slug?.length === 1) {
+        return <h1>View Docs for Feature {params.slug[0]}</h1>;
+    }
+    else {
+        return <h1>Docs Home Page</h1>;
+    }
+}
+
+export default Docs;
