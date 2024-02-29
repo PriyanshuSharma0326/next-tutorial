7460a88e38cfa673321814fcdfc401ebd0923182
PriyanshuSharma0326 <ps940016@gmail.com>
2024-02-29 20:10:57 +0530

    fix error

--- src/app/docs/[[...slug]]/page.js
+++ src/app/docs/[[...slug]]/page.js
@@ -5,9 +5,7 @@ function Docs({ params }) {
     else if (params.slug?.length === 1) {
         return <h1>View Docs for Feature {params.slug[0]}</h1>;
     }
-    else {
-        return <h1>Docs Home Page</h1>;
-    }
+    return <h1>Docs Home Page</h1>;
 }
 
 export default Docs;
