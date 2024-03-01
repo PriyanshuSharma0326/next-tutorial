a4f2e8f337c321928169385e87f0c2954323d303
PriyanshuSharma0326 <ps940016@gmail.com>
2024-03-01 13:50:54 +0530

    nested layouts

---
+++ src/app/products/[productID]/layout.js
@@ -0,0 +1,11 @@
+function ProductsDetailsLayout({ children }) {
+    return (
+        <>
+            {children}
+            <h2>Featured Products</h2>
+            {/* Carousel Here */}
+        </>
+    );
+}
+
+export default ProductsDetailsLayout;
