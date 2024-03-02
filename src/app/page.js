7326c4296430439f03acc0e2a339b69c2e586da9
PriyanshuSharma0326 <ps940016@gmail.com>
2024-03-02 15:35:13 +0530

    Link component navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 function Home() {
-    return <h1>Welcome Home!</h1>;
+    return (
+        <>
+            <h1>Welcome Home!</h1>
+
+            <ul>
+                <li>
+                    <Link href='/blog'>Blog</Link>
+                </li>
+                <li>
+                    <Link href='/products'>Products</Link>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default Home;
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,12 +1,24 @@
+import Link from "next/link";
+
 function ProductsList() {
-    return <>
-        <h1>List of products!</h1>
-        <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-        </ul>
-    </>;
+    return (
+        <>
+            <h1>List of products!</h1>
+            <ul>
+                <li>
+                    <Link href='products/1'>Product 1</Link>
+                </li>
+                <li>
+                    <Link href='products/2'>Product 2</Link>
+                </li>
+                <li>
+                    {/* The replace prop replaces the current history */}
+                    <Link href='products/3' replace>Product 3</Link>
+                </li>
+            </ul>
+            <Link href='/'>Home</Link>
+        </>
+    );
 }
 
 export default ProductsList;
