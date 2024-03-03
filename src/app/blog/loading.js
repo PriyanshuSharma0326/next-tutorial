51c1fd2e156e3bee74e1b601a2a3ad095eb53614
PriyanshuSharma0326 <ps940016@gmail.com>
2024-03-03 07:47:26 +0530

    loading ui with loading.js

---
+++ src/app/blog/loading.js
@@ -0,0 +1,9 @@
+function BlogLoading() {
+    return (
+        <>
+            <h1 className="text-[2rem] text-black font-semibold">Loading...</h1>
+        </>
+    )
+}
+
+export default BlogLoading;
